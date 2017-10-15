9b10a45366bc44caa1141712b356ab49693f4fa3
ExcaliburBunker <excalibur@bunkerlabs.us>
2017-10-15 11:44:20 -0700

    Loop and While

--- Java WorkSpace.js
+++ Java WorkSpace.js
@@ -286,14 +286,31 @@ function newFunction() {
     console.log(john);
     */
     // Lecture: Loops
+    /*
     for (var i = 0; i < 10; i++) {
         console.log(i);
     }
+
     var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
     for (var i = 0; i < names.length; i++) {
         console.log(names[i]);
     }
     for (var i = names.length - 1; i >= 0; i--) {
         console.log(names[i]);
+    }
+    */
+    var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+
+    var i = 0;
+    while(i < names.length) {
+        console.log(names[i]);
+        i++;
+    }
+
+    for (var i = 0; i <= 5; i++) {
+        if( i === 3) {
+            continue;
+        }
+        console.log(i)
     }
 }
